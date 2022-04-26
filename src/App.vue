df4bdec28eaf9dfe30a45756ccf0f82f501ad778
czh <1562641145@qq.com>
2022-04-26 17:07:00 +0800

    添加备案信息和新增网站

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
 
 <template>
   <router-view></router-view>
+  <div class="fixed z-50 bottom-0 w-full bg-white md:text-base text-sm shadow-inner">
+    <div class="flex justify-center leading-6">
+      <span class="text-gray-600">Copyright © 2022</span>
+      <a class="tracking-wide text-blue-500 ml-2" href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2022045649号-1</a>
+    </div>
+  </div>
 </template>
 
 <style>
